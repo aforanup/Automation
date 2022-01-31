ce9d22db4d8c7625200507763eda661bd1f2ecc6
aforanup <aforanupn@gmail.com>
2022-01-31 15:50:55 +0545

    email app

--- email_app.py
+++ email_app.py
@@ -1,3 +1,22 @@
 import smtplib
 from email.message import EmailMessage
 import ssl
+
+
+subject = 'Test'
+sender = 'aforanupn@gmail.com'
+receiver = 'aforanupn@gmail.com'
+password = 'amazeme123'
+content = 'This is a test message'
+
+message = EmailMessage()
+message['Subject'] = subject
+message['To'] = receiver
+message['From'] = sender
+message.set_content(content)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
+    server.login(sender, password)
+    server.sendmail(sender, receiver, message, message.as_string())
